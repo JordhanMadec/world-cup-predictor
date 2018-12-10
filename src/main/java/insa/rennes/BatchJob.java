729e98a6cd0142a8af0e245bc7c363a118a37d76
Jordhan <jordhan.madec@gmail.com>
2018-12-10 12:45:15 +0100

    Read csv file

--- src/main/java/insa/rennes/BatchJob.java
+++ src/main/java/insa/rennes/BatchJob.java
@@ -18,49 +18,45 @@
 
 package insa.rennes;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple6;
+import org.apache.flink.util.Collector;
 
-/**
- * Skeleton for a Flink Batch Job.
- *
- * <p>For a tutorial how to write a Flink batch application, check the
- * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution,
- * change the main class in the POM.xml file to this class (simply search for 'mainClass')
- * and run 'mvn clean package' on the command line.
- */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BatchJob {
+
+	static final String fifaRanksPath = "/Users/jordhanmadec/dev/INSA/world-cup-predictor/fifa_ranking.csv";
+	static final String worldcupHistoryPath = "";
+	static final String worldcupGamesPath = "";
+	static final String internationalGamesPath = "/Users/jordhanmadec/dev/INSA/world-cup-predictor/international_results.csv";
+
 
 	public static void main(String[] args) throws Exception {
 		// set up the batch execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataSet<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/batch/index.html
-		 *
-		 * and the examples
-		 *
-		 * http://flink.apache.org/docs/latest/apis/batch/examples.html
-		 *
-		 */
+		DataSet<Tuple6<Integer, String, Float, Float, Integer, Date>> fifaRanks = env.readCsvFile(fifaRanksPath)
+				.ignoreFirstLine()
+				.types(Integer.class, String.class, Float.class, Float.class, Integer.class, String.class)
+				.flatMap(new DateConverter());
 
-		// execute program
+		fifaRanks.print();
+
 		env.execute("Flink Batch Java API Skeleton");
+	}
+
+	public static class DateConverter implements FlatMapFunction<Tuple6<Integer, String, Float, Float, Integer, String>, Tuple6<Integer, String, Float, Float, Integer, Date>> {
+
+		@Override
+		public void flatMap(Tuple6<Integer, String, Float, Float, Integer, String> in, Collector<Tuple6<Integer, String, Float, Float, Integer, Date>> out) throws Exception {
+			DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+			out.collect(new Tuple6<Integer, String, Float, Float, Integer, Date>(in.f0, in.f1, in.f2, in.f3, in.f4, format.parse(in.f5)));
+		}
 	}
 }
