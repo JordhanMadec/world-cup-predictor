6038cfa8fc2416cd6ed7e57cf919d16465fc010a
Jordhan <jordhan.madec@gmail.com>
2018-12-17 09:10:42 +0100

    Count victories

--- src/main/java/insa/rennes/BatchJob.java
+++ src/main/java/insa/rennes/BatchJob.java
@@ -18,11 +18,12 @@
 
 package insa.rennes;
 
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple6;
+import org.apache.flink.api.java.tuple.Tuple9;
 import org.apache.flink.util.Collector;
 
 import java.text.DateFormat;
@@ -34,7 +35,7 @@ public class BatchJob {
 	static final String fifaRanksPath = "/Users/jordhanmadec/dev/INSA/world-cup-predictor/fifa_ranking.csv";
 	static final String worldcupHistoryPath = "";
 	static final String worldcupGamesPath = "";
-	static final String internationalGamesPath = "/Users/jordhanmadec/dev/INSA/world-cup-predictor/international_results.csv";
+	static final String internationalResultsPath = "/Users/jordhanmadec/dev/INSA/world-cup-predictor/international_results.csv";
 
 
 	public static void main(String[] args) throws Exception {
@@ -44,19 +45,64 @@ public class BatchJob {
 		DataSet<Tuple6<Integer, String, Float, Float, Integer, Date>> fifaRanks = env.readCsvFile(fifaRanksPath)
 				.ignoreFirstLine()
 				.types(Integer.class, String.class, Float.class, Float.class, Integer.class, String.class)
-				.flatMap(new DateConverter());
+				.flatMap(new FifaRankingDateConverter());
 
-		fifaRanks.print();
+		DataSet<Tuple3<String, String, Integer>> internationalResults = env.readCsvFile(internationalResultsPath)
+				.ignoreFirstLine()
+				.types(String.class, String.class, String.class, Integer.class, Integer.class, String.class, String.class, String.class, Boolean.class)
+				.flatMap(new InternationalResultsDateConverter())
+				.flatMap(new InternationalResultsStats())
+				.groupBy(0, 1)
+				.sum(2);
 
-		env.execute("Flink Batch Java API Skeleton");
+		internationalResults.print();
+
+		env.execute("Worldcup Predictor");
 	}
 
-	public static class DateConverter implements FlatMapFunction<Tuple6<Integer, String, Float, Float, Integer, String>, Tuple6<Integer, String, Float, Float, Integer, Date>> {
-
+	public static class FifaRankingDateConverter implements FlatMapFunction<Tuple6<Integer, String, Float, Float, Integer, String>, Tuple6<Integer, String, Float, Float, Integer, Date>> {
 		@Override
 		public void flatMap(Tuple6<Integer, String, Float, Float, Integer, String> in, Collector<Tuple6<Integer, String, Float, Float, Integer, Date>> out) throws Exception {
 			DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
-			out.collect(new Tuple6<Integer, String, Float, Float, Integer, Date>(in.f0, in.f1, in.f2, in.f3, in.f4, format.parse(in.f5)));
+			out.collect(new Tuple6(in.f0, in.f1, in.f2, in.f3, in.f4, format.parse(in.f5)));
+		}
+	}
+
+	public static class InternationalResultsDateConverter implements FlatMapFunction<Tuple9<String, String, String, Integer, Integer, String, String, String, Boolean>, Tuple9<Date, String, String, Integer, Integer, String, String, String, Boolean>> {
+		@Override
+		public void flatMap(Tuple9<String, String, String, Integer, Integer, String, String, String, Boolean> in, Collector<Tuple9<Date, String, String, Integer, Integer, String, String, String, Boolean>> out) throws Exception {
+			DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+			out.collect(new Tuple9(format.parse(in.f0), in.f1, in.f2, in.f3, in.f4, in.f5, in.f6, in.f7, in.f8));
+		}
+	}
+
+	public static class InternationalResultsStats implements FlatMapFunction<Tuple9<Date, String, String, Integer, Integer, String, String, String, Boolean>, Tuple3<String, String, Integer>> {
+		@Override
+		public void flatMap(Tuple9<Date, String, String, Integer, Integer, String, String, String, Boolean> in, Collector<Tuple3<String, String, Integer>> out) throws Exception {
+			if (in.f3 > in.f4) {
+				out.collect(new Tuple3(in.f1, "win", 1));
+				out.collect(new Tuple3(in.f2, "loss", 1));
+				if (in.f5.equals("FIFA World Cup")) {
+					out.collect(new Tuple3(in.f1, "wc_win", 1));
+					out.collect(new Tuple3(in.f2, "wc_loss", 1));
+				}
+			} else if (in.f3 < in.f4) {
+				out.collect(new Tuple3(in.f2, "win", 1));
+				out.collect(new Tuple3(in.f1, "loss", 1));
+				if (in.f5.equals("FIFA World Cup")) {
+					out.collect(new Tuple3(in.f2, "wc_win", 1));
+					out.collect(new Tuple3(in.f1, "wc_loss", 1));
+				}
+			} else {
+				out.collect(new Tuple3(in.f1, "draw", 1));
+				out.collect(new Tuple3(in.f2, "draw", 1));
+				if (in.f5.equals("FIFA World Cup")) {
+					out.collect(new Tuple3(in.f1, "wc_draw", 1));
+					out.collect(new Tuple3(in.f2, "wc_draw", 1));
+				}
+			}
+
+			//out.collect(new Tuple3(in.f1, "", in.f3));
 		}
 	}
 }
