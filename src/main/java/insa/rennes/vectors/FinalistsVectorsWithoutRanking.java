f55b82753326f808e24fa468ffff8e4eef1529f7
Jordhan <jordhan.madec@gmail.com>
2019-01-03 18:18:24 +0100

    idem

--- src/main/java/insa/rennes/vectors/FinalistsVectorsWithoutRanking.java
+++ src/main/java/insa/rennes/vectors/FinalistsVectorsWithoutRanking.java
@@ -1,20 +1,19 @@
 package insa.rennes.vectors;
 
 import org.apache.flink.api.common.functions.JoinFunction;
-import org.apache.flink.api.java.tuple.Tuple10;
+import org.apache.flink.api.java.tuple.Tuple8;
 import org.apache.flink.api.java.tuple.Tuple5;
-import org.apache.flink.api.java.tuple.Tuple7;
 
 public class FinalistsVectorsWithoutRanking implements JoinFunction<
-        Tuple7<String, Integer, Double, Integer, Double, Double, Double>,
+        Tuple5<String, Integer, Double, Double, Double>,
         Tuple5<String, Integer, Integer, Integer, Double>,
-        Tuple10<String, Integer, Double, Integer, Double, Double, Double, Integer, Integer, Double>> {
+        Tuple8<String, Integer, Double, Double, Double, Integer, Integer, Double>> {
 
     @Override
-    public Tuple10<String, Integer, Double, Integer, Double, Double, Double, Integer, Integer, Double> join(
-            Tuple7<String, Integer, Double, Integer, Double, Double, Double> in1,
+    public Tuple8<String, Integer, Double, Double, Double, Integer, Integer, Double> join(
+            Tuple5<String, Integer, Double, Double, Double> in1,
             Tuple5<String, Integer, Integer, Integer, Double> in2
     ) throws Exception {
-        return new Tuple10(in1.f0, in1.f1, in1.f2, in1.f3, in1.f4, in1.f5, in1.f6, in2.f2, in2.f3, in2.f4);
+        return new Tuple8(in1.f0, in1.f1, in1.f2, in1.f3, in1.f4, in2.f2, in2.f3, in2.f4);
     }
 }
