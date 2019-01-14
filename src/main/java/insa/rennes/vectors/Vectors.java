08e3a35b41c8ebec35c1400b58232b0a7b94f83f
Jordhan <jordhan.madec@gmail.com>
2019-01-14 11:35:30 +0100

    Centralize weights settings

--- src/main/java/insa/rennes/vectors/Vectors.java
+++ src/main/java/insa/rennes/vectors/Vectors.java
@@ -1,5 +1,6 @@
 package insa.rennes.vectors;
 
+import insa.rennes.Settings;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.api.java.tuple.Tuple6;
@@ -16,6 +17,13 @@ public class Vectors implements JoinFunction<
             Tuple5<String, Integer, Double, Double, Double> in2
     ) throws Exception {
         // (rank weight, win ratio, loss ratio, goals ratio, finals ratio, semi finals ratio, hosting)
-        return new Tuple9(in1.f0, in1.f1, in1.f2, in1.f3, in1.f4, in1.f5, in2.f2, in2.f3, in2.f4 * 1.0);
+        return new Tuple9(in1.f0, in1.f1,
+                in1.f2 * Settings.RANK_WEIGHT,
+                in1.f3 * Settings.WIN_WEIGHT,
+                in1.f4 * Settings.LOSS_WEIGHT,
+                in1.f5 * Settings.GOALS_WEIGHT,
+                in2.f2 * Settings.FINAL_WEIGHT,
+                in2.f3 * Settings.SEMI_FINAL_WEIGHT,
+                in2.f4 * Settings.HOSTING_WEIGHT);
     }
 }
